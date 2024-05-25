fcd876c3bbe39fd2fd8fef5d0eb77e96b7bfda96
arushtripathi121 <arushtripathi121@gamail.com>
2024-05-26 01:47:13 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Body from './Components/Body';
 import appStore from './utils/appStore';
 
 function App() {
+  
   return (
     <div>
       <Provider store={appStore}>
--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Browse from './Browse';
 import { createBrowserRouter } from 'react-router-dom';
 import { RouterProvider } from 'react-router-dom';
 import LoginPage from './LoginPage';
 import HomePage from './HomePage';
+import { auth } from '../utils/Firebase';
+import { onAuthStateChanged } from 'firebase/auth';
+import { useDispatch } from 'react-redux';
+import { addUser, removeUser } from '../utils/userSlice';
 const Body = () => {
+
+  const dispatch = useDispatch();
 
   const appRouter = createBrowserRouter([
     {
@@ -19,7 +25,24 @@ const Body = () => {
       path: '/login',
       element: <LoginPage />
     },
-  ])
+  ]);
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        // User is signed in, see docs for a list of available properties
+        // https://firebase.google.com/docs/reference/js/auth.user
+        const {uid, email, displayName} = user.uid;
+        dispatch(addUser({uid:uid, email:email, displayName:displayName}));
+        // ...
+      } else {
+        // User is signed out
+        // ...
+        dispatch(removeUser())
+      }
+    });
+  }, [])
+
   return (
     <div>
       <div className=' '>

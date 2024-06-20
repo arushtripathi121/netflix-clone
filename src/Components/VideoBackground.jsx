13ec022f8ebcf2d07e486d32ead991c1dc54f627
arushtripathi121 <arushtripathi121@gamail.com>
2024-06-20 17:42:04 +0530

    commit

--- src/Components/VideoBackground.jsx
+++ src/Components/VideoBackground.jsx
@@ -11,7 +11,7 @@ const VideoBackground = ({ movieId }) => {
     <div className="w-full">
       <iframe
         className="w-full aspect-video"
-        src={`https://www.youtube.com/embed/${trailerVideo?.key}?autoplay=1&mute=0&controls=0&loop=1&playlist=${trailerVideo?.key}&rel=0&modestbranding=1&playsinline=1&iv_load_policy=3&disablekb=1&showinfo=0`}
+        src={`https://www.youtube.com/embed/${trailerVideo?.key}?autoplay=1&mute=1&controls=0&loop=1&playlist=${trailerVideo?.key}&rel=0&modestbranding=1&playsinline=1&iv_load_policy=3&disablekb=1&showinfo=0`}
         title="YouTube video player"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
       ></iframe>
